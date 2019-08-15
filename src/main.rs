58fbcff590f12e38118c8512b178e71ef8283ac9
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-08-15 20:52:11 +0900

    feat: error handling in main fun

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+extern crate exitfailure;
+extern crate failure;
+
+use exitfailure::ExitFailure;
+
+fn main() -> Result<(), ExitFailure> {
+    return Ok(run()?);
+}
+
+fn run() -> Result<(), failure::Error> {
+    return Ok(());
 }
